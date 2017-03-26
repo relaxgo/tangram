839e4210c9f955c4aa9e2cd4696ede1544341a04
relaxgo <patientzhe@gmail.com>
2017-03-26 13:51:52 +0800

    add more basic types support

--- param/param.go
+++ param/param.go
@@ -16,19 +16,71 @@ func String(vs ValueStore, key string) string {
 	return vs.Value(key)
 }
 
+func Bool(vs ValueStore, key string) bool {
+	v := String(vs, key)
+	return strings.ToLower(v) == "true"
+}
+
 func Int(vs ValueStore, key string) int {
-	v, _ := strconv.Atoi(String(vs, key))
+	v, _ := strconv.ParseInt(String(vs, key), 10, 0)
+	return int(v)
+}
+
+func Int8(vs ValueStore, key string) int8 {
+	v, _ := strconv.ParseInt(String(vs, key), 10, 8)
+	return int8(v)
+}
+
+func Int16(vs ValueStore, key string) in4 {
+	v, _ := strconv.ParseInt(String(vs, key), 10, 16)
+	return int16(v)
+}
+func Int32(vs ValueStore, key string) int {
+	v, _ := strconv.ParseInt(String(vs, key), 10, 32)
+	return int32(v)
+}
+
+func Int64(vs ValueStore, key string) int {
+	v, _ := strconv.ParseInt(String(vs, key), 10, 64)
 	return v
+}
+
+func Uint(vs ValueStore, key string) int {
+	v, _ := strconv.ParseUint(String(vs, key), 10, 0)
+	return int(v)
+}
+
+func Uint8(vs ValueStore, key string) int8 {
+	v, _ := strconv.ParseUint(String(vs, key), 10, 8)
+	return int8(v)
+}
+
+func Uint16(vs ValueStore, key string) in4 {
+	v, _ := strconv.ParseUint(String(vs, key), 10, 16)
+	return int16(v)
+}
+func Uint32(vs ValueStore, key string) int {
+	v, _ := strconv.ParseUint(String(vs, key), 10, 32)
+	return int32(v)
+}
+
+func Uint64(vs ValueStore, key string) int {
+	v, _ := strconv.ParseUint(String(vs, key), 10, 64)
+	return v
+}
+
+func Byte(vs ValueStore, key string) byte {
+	byte(Uint8(vs, key))
+}
+
+func Float32(p ValueStore, key string) float32 {
+	v, _ := strconv.ParseFloat(String(p, key), 32)
+	return float32(v)
 }
 
 func Float64(p ValueStore, key string) float64 {
 	v, _ := strconv.ParseFloat(String(p, key), 64)
 	return v
-}
-
-func Bool(vs ValueStore, key string) bool {
-	v := String(vs, key)
-	return strings.ToLower(v) == "true"
 }
 
 type FileSize interface {
