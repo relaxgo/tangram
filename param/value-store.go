8764014f08bb57aab242248dc47ad8a8883d7d72
relaxgo <patientzhe@gmail.com>
2018-04-04 12:47:05 +0800

    fix read body

--- param/value-store.go
+++ param/value-store.go
@@ -1,6 +1,8 @@
 package param
 
 import (
+	"bytes"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -21,15 +23,23 @@ func (r *valueStore) Value(key string) string {
 	contentType := GetContentType(r.Request)
 	if contentType == "application/json" {
 		defer r.Body.Close()
-		body, err := ioutil.ReadAll(r.Body)
+		r1, r2, err := drainBody(r.Body)
+		if err != nil {
+			log.Print("read failed", err)
+			return ""
+		}
+		r.Body = r2
+		body, err := ioutil.ReadAll(r1)
 		// TODO add new body reader ?
 		if err != nil {
 			log.Print("read body", err)
+			return ""
 		}
 		value, _, _, err := jsonparser.Get(body, key)
 
 		if err != nil {
 			log.Println("jsonparser", err)
+			return ""
 		}
 		if len(value) > 0 {
 			return string(value)
@@ -47,4 +57,20 @@ func (r *valueStore) Value(key string) string {
 func GetContentType(r *http.Request) string {
 	s := r.Header.Get("Content-Type")
 	return strings.TrimSpace(strings.Split(s, ";")[0])
+}
+
+// copy from golang.org/src/net/http/httputil/dump.go
+func drainBody(b io.ReadCloser) (r1, r2 io.ReadCloser, err error) {
+	if b == http.NoBody {
+		// No copying needed. Preserve the magic sentinel meaning of NoBody.
+		return http.NoBody, http.NoBody, nil
+	}
+	var buf bytes.Buffer
+	if _, err = buf.ReadFrom(b); err != nil {
+		return nil, b, err
+	}
+	if err = b.Close(); err != nil {
+		return nil, b, err
+	}
+	return ioutil.NopCloser(&buf), ioutil.NopCloser(bytes.NewReader(buf.Bytes())), nil
 }
